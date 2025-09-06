b3538d06fba813dd0214ac574bbce0b1428eabfb
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-09-06 16:32:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -130,7 +130,7 @@ SEASONS = [f"season {i}" for i in range(1, 23)]
 START_IMG = (
     environ.get(
         "START_IMG",
-        "https://i.ibb.co/qpxpGmC/image.jpg https://i.ibb.co/DQ35zLZ/image.jpg",
+        "https://envs.sh/iRy.jpg https://envs.sh/iRX.jpg",
     )
 ).split()
 FORCESUB_IMG = environ.get("FORCESUB_IMG", "https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg")
