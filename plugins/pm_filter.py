cbb4a3198919e13ba3fee71893a4b06eef6c0f25
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-09-07 11:39:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1537,7 +1537,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [
             [
                 InlineKeyboardButton(
-                    "ʀᴇᴘᴏ", url="https://to.me/aonemarathi"
+                    "ʀᴇᴘᴏ", url="https://t.me/aonemarathi"
                 )
             ],
             [
