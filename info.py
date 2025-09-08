bb7c7af2b935a39d5a5dd57c0e8849d9e9a6aac6
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-09-08 20:19:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ REQUEST_CHANNEL = (
     if request_channel and id_pattern.search(request_channel)
     else None
 )
-MOVIE_UPDATE_CHANNEL = int(environ.get("MOVIE_UPDATE_CHANNEL", ""))
+MOVIE_UPDATE_CHANNEL = int(environ.get("MOVIE_UPDATE_CHANNEL", "-1002328506195"))
 
 # Added Link Here Not Id
 SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/MzBotzSupport")
